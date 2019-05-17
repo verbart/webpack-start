fc68da9b0a92fd85e311529404259b845218d083
Artem Verbat <artem.verbat@gmail.com>
2019-05-17 11:21:33 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+### Install dependencies
+
+* In root folder run console command: `npm install`
+
+### Developing
+
+- Run `npm start` to start the development server.  
+   It should automatically open the client in your browser when ready.
+
+### Build
+
+- Run `npm run build` for create production build.
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    open: true,
+    port: 8080,
+    overlay: true
+  },
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.hbs$/,
+        use: [
+          'handlebars-loader',
+          'extract-loader',
+          {
+            loader: 'html-loader',
+            options: {
+              interpolate: true
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/images/[name].[hash].[ext]'
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.hbs',
+      templateParameters: {
+        ENV: {
+          message: 'Hello'
+        }
+      },
+      inject: 'head',
+      hash: true,
+      cache: false
+    }),
+
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]
+};
